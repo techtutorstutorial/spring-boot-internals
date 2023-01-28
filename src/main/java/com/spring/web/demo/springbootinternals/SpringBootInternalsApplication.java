db9bc8187bab6095b7b67649a52a6e6863f58a3f
Madhusudan Yadav <techtutorstutorial@gmail.com>
2023-01-28 16:07:47 +0530

    inital commit

---
+++ src/main/java/com/spring/web/demo/springbootinternals/SpringBootInternalsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.web.demo.springbootinternals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInternalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInternalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/web/demo/springbootinternals/SpringBootInternalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web.demo.springbootinternals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInternalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
